7871ce94a6a18619b08126cfd9c911a3c4879feb
sam_gustavson <Senkevi44@gmail.com>
2018-08-07 16:54:25 +0300

    refactoring Contacts app

--- src/main/java/Contacts.java
+++ src/main/java/Contacts.java
@@ -7,39 +7,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Contacts {
-    private static final String SAMPLE_CSV_FILE_PATH = "/Users/oleksandr_senkevych/Downloads/CSVParser/src/main/resources/contacts.csv";
+    private static final String SAMPLE_CSV_FILE_PATH = "src/main/resources/contacts.csv";
+    Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
 
-//    public Contacts(String name) {
-//        this.name = name;
-//    }
-//
-//    public Contacts() throws IOException {
-//        try (
-//                Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
-//                CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
-//        ) {
-//            for (CSVRecord csvRecord : csvParser) {
-//                // Accessing Values by Column Index
-//                String name = csvRecord.get(0);
-//                String email = csvRecord.get(1);
-//                String phone = csvRecord.get(2);
-//                String country = csvRecord.get(3);
-//
-//                System.out.println("Record No - " + csvRecord.getRecordNumber());
-//                System.out.println("---------------");
-//                System.out.println("Name : " + name);
-//                System.out.println("Email : " + email);
-//                System.out.println("Phone : " + phone);
-//                System.out.println("Country : " + country);
-//                System.out.println("---------------\n\n");
-//
-//            }
-//        }
-//    }
-//}
+    public Contacts() throws IOException {
+    }
+    public List<String[]> searchByText(String cache) throws IOException {
 
-    public List<String[]> name(String cache) throws IOException {
-        Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
         CSVReader csvReader = new CSVReader(reader);
 
         List<String[]> records = csvReader.readAll();
